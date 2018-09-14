b7497d798f71b110fb57f13fcf7c235787e9660d
HeenaSayyad <heenapatel3090@gmail.com>
2018-09-14 15:18:00 +0530

    Scroll View Demo is simple of scroll bar. where I have two buttons and using scrollview on view controller and the height of view controller is been extended by 1000. when we see the ScrollView Demo there is one button when we scroll up and second button when scroll down.

---
+++ ScrollView/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ScrollView
+//
+//  Created by ramjan sayyad on 14/09/18.
+//  Copyright © 2018 ramjan sayyad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func hi(_ sender: Any) {
+        
+        print("HI")
+    }
+    
+   
+    @IBAction func swift(_ sender: Any)
+    
+    {
+        print("To Swift")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
